582f74e4bcf164dcdd28027e0da96be331a808e6
atidele <alejandro.tidele@mercadolibre.com>
2020-03-31 21:36:30 -0300

    FIX - TP1 - EJ2

--- src/TP1_EJ2/Models/Empleado.java
+++ src/TP1_EJ2/Models/Empleado.java
@@ -1,45 +1,35 @@
 package TP1_EJ2.Models;
 
 public class Empleado {
-	
 	//Atributos
-	
-	private int id;
+	private final int id; // Final porque no tiene que poder ser cambiado una vez establecido
 	private String nombre;
 	private int edad;
-	
 	private static int contador = 1000; // variable de la clase 
 	
 	//Constructores
-	
-	public Empleado()
-	{
-		this.id = contador++;
-		this.nombre = "Pepe";
-		this.edad = 30;
+	public Empleado() {
+		this("pepe", 30);
 	}
 	
-	public Empleado(String nombre,int edad)
-	{
-		this.id = contador ++;
+	public Empleado(String nombre,int edad) {
+		this.id = contador++; // Asigna y despues suma
 		this.nombre = nombre;
 		this.edad = edad;
 	}
 	
 	//Getters and Setters
-	
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public int getEdad() {
 		return edad;
 	}
@@ -49,11 +39,10 @@ public class Empleado {
 	
 	//Metodos
 	public String DevuelveDatos() {
-		return String.format("Empleado: ID = %d, Nombre = %s, Edad = %s",id,nombre,edad);;
+		return String.format("Empleado: ID = %d, Nombre = %s, Edad = %s", this.id, this.nombre, this.edad);
 	}
 	
 	public static int DevuelveProximoId() {
-		return contador++ ;
+		return Empleado.contador; // Solo el contador en este momento, ya que el constructor ya lo incrementó
 	}
-	
 }
